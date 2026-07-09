3f554fcd99b23f0e233f0194e98e214b8960b5e5
yusma-hassan <yusmahassan1@gmail.com>
2026-07-09 19:06:09 +0500

    created model class for json response

---
+++ lib/Models/posts_model.dart
@@ -0,0 +1,35 @@
+
+class User{
+
+  final int userId;
+  final int id;
+  final String title;
+  final String  body;
+
+  User({required this.userId,
+  required this.id,
+  required this.title,
+  required this.body});
+
+  factory User.fromJson(Map<String,dynamic> json){
+    return User(
+      userId: json["userId"], 
+      id: json["id"],
+      title: json["title"], 
+      body: json["body"]);
+  }
+
+Map<String,dynamic> toJson(){
+
+return {
+
+"userId":userId,
+"id":id,
+"title" : title,
+"body" : body
+};
+
+}
+
+} 
+
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+       
+        title: Text("Api Course"),
+      ),
+      body: Center(
+        
+    ),
+    );
+  }
+}
